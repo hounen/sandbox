967d7897c526a0855ea93f91c3042abe4fdb5e29
hounen <nam.pma@nifty.com>
2024-09-04 21:44:17 +0900

    a

--- python/bucket2lab/bucket2lab.py
+++ python/bucket2lab/bucket2lab.py
@@ -1,9 +1,32 @@
+
+
 # ローカルのGitBucketをGitHubに移行する（その２）
 # https://kwatanabe.hatenablog.jp/entry/2020/10/11/130324#GitBucket-%E3%81%8B%E3%82%89-Issue-%E3%82%92%E5%8F%96%E3%82%8A%E5%87%BA%E3%81%99
 # ローカルのGitBucketをGitHubに移行する（その３）
 # https://kwatanabe.hatenablog.jp/entry/2020/10/22/191134
 
 # from GitBucket
+
+# issueのリストを取得する
+# issueを登録する
+# commentを登録する
+
+# APIにリクエストを送信
+response = requests.get(url)
+
+# レスポンスが正常か確認
+if response.status_code == 200:
+    # JSONデータをパース
+    data = response.json()
+    # パースしたデータを表示
+    print(data)
+else:
+    print(f"エラー: {response.status_code}")
+
+# 名前にアクセス
+print(data['name'])  # John
+# 年齢にアクセス
+print(data['age'])   # 30
 
 # to GitLab
 issues = project.issues.list()
