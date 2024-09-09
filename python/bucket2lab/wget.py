37e86e28f5b95dd13347f6f8964b4e5846d0b360
hounen <nam.pma@nifty.com>
2024-09-09 22:07:52 +0900

    a

--- python/bucket2lab/wget.py
+++ python/bucket2lab/wget.py
@@ -16,37 +16,47 @@
 
 import subprocess
 
-# 1. ログインしてクッキーを取得するPowerShellコマンド
-login_url = "http://localhost:8081/signin?redirect=%2F"
-username = "root"
-password = "root"
-login_command = f"""
-$loginUrl = '{login_url}';
-$loginForm = @{{'userName' = '{username}'; 'password' = '{password}'}};
-$response = Invoke-WebRequest -Uri $loginUrl -Method Post -Body $loginForm -SessionVariable session;
-$session;
-"""
+def download_file(login_url, username, password, base_url, output_files):
+    for output_file in output_files:
+        download_command = f""
+        # 1. ログインしてクッキーを取得するPowerShellコマンド
+        download_command += f"""
+        $loginUrl = '{login_url}';
+        $loginForm = @{{'userName' = '{username}'; 'password' = '{password}'}};
+        $response = Invoke-WebRequest -Uri $loginUrl -Method Post -Body $loginForm -SessionVariable session;
+        $session;
+        """
+        # 2. 画像ファイルをダウンロードするPowerShellコマンド
+        download_command += f"""
+        $url = '{base_url + output_file}';
+        $out = '{output_file + ".png"}';
+        Invoke-WebRequest -Uri $url -OutFile $out -WebSession $session;
+        """
+        # 3. PowerShellコマンドをPythonから実行する
+        powershell_command = f"{download_command}"
+        result = subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
 
-# 2. 画像ファイルをダウンロードするPowerShellコマンド
-image_url = "http://localhost:8081/root/test/_attached/1725631935090YsLGq7UGEG"
-output_file = "1725631935090YsLGq7UGEG.png"
-download_command = f"""
-$url = '{image_url}';
-$out = '{output_file}';
-Invoke-WebRequest -Uri $url -OutFile $out -WebSession $session;
-"""
+        # 実行結果を確認
+        if result.returncode == 0:
+            print("Download successful.")
+            print(result.stdout)
+        else:
+            print("Error occurred:")
+            with open('stderr.html', 'w') as f:
+                f.write(result.stderr)
+            exit()
 
-# 3. PowerShellコマンドをPythonから実行する
-powershell_command = f"{login_command} {download_command}"
-result = subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+if __name__ == '__main__':
 
-# 実行結果を確認
-if result.returncode == 0:
-    print("Download successful.")
-    print(result.stdout)
-else:
-    print("Error occurred:")
-    print(result.stderr)
+    login_url = "http://localhost:8081/signin?redirect=%2F"
+    username = "root"
+    password = "root"
+    base_url = "http://localhost:8081/root/test/_attached/"
+    output_files = [
+        "1725631935090YsLGq7UGEG"
+        , "1725884427340NvaVGiSawl"
+        ]
+    download_file(login_url, username, password, base_url, output_files)
 
 # 1. Privateでファイルをダウンロードする方法の検証
 # 2. (issue説明とコメントの)リンク置換といったん最初から通しで処理が走るか検証(骨格を決める)
